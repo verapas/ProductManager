685c0d64351b583e6652f626c82757543f2ff82f
Pascal Veraguth <pascal.veraguth@hotmail.ch>
2024-09-23 17:41:02 +0200

    inital commit

---
+++ src/main/java/ch/csbe/productmanager/resources/ProductManagerApplication.java
@@ -0,0 +1,14 @@
+package ch.csbe.productmanager.resources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductManagerApplication.class, args);
+    }
+
+
+}
