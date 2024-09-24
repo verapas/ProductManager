2a820d5add875973500aebf15a654c6403b27d47
Pascal Veraguth <pascal.veraguth@hotmail.ch>
2024-09-24 15:40:37 +0200

    Add CategoryService and ProductService

--- src/main/java/ch/csbe/productmanager/ressources/category/CategoryService.java
+++ src/main/java/ch/csbe/productmanager/ressources/category/CategoryService.java
@@ -1,4 +1,38 @@
 package ch.csbe.productmanager.ressources.category;
 
+import ch.csbe.productmanager.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class CategoryService {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    public Category findById(Long id) {
+        return this.categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category with the id " + id + " could not be found!"));
+    }
+
+    public List<Category> findAll() {
+        return this.categoryRepository.findAll();
+    }
+
+    public Category create(Category categoryToSave) {
+        return this.categoryRepository.save(categoryToSave);
+    }
+
+    public Category update(Long id, Category categoryToUpdate) {
+        Category category = this.categoryRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Category with the id " + id + " could not be found!"));
+        category.setName(categoryToUpdate.getName());
+        return categoryRepository.save(category);
+    }
+
+    public void delete(Long id) {
+        categoryRepository.deleteById(id);
+    }
 }
